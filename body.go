7c30260afeeae8c26c16a785b57cf44444c7db2a
hwcer <hwclegend@163.com>
2022-03-14 16:56:09 +0800

    develop

--- body.go
+++ body.go
@@ -21,13 +21,16 @@ func (b *Body) release() {
 }
 
 func (b *Body) Len() int {
+	if _,err := b.Bytes();err!=nil{
+		return 0
+	}
 	return len(b.bytes)
 }
 
 func (b *Body) Get(key string) (val interface{}, ok bool) {
 	if b.params == nil {
 		params := make(map[string]interface{}, 0)
-		if err := b.c.Bind(params); err != nil {
+		if err := b.c.Bind(&params); err != nil {
 			logger.Error("BODY BIND Err:%v", err)
 		}
 		b.params = params
@@ -37,16 +40,19 @@ func (b *Body) Get(key string) (val interface{}, ok bool) {
 }
 
 func (b *Body) Read(p []byte) (n int, err error) {
-	if b.bytes == nil {
-		b.bytes, err = io.ReadAll(b.c.Request.Body)
-	}
-	if err != nil {
+	if _,err = b.Bytes();err!=nil{
 		return
 	}
 	n = copy(p, b.bytes)
 	return
 }
 
-func (b *Body) Bytes() []byte {
-	return b.bytes
+func (b *Body) Bytes() ([]byte,error) {
+	if b.bytes == nil {
+		var err error
+		if b.bytes, err = io.ReadAll(b.c.Request.Body);err!=nil{
+			return nil,err
+		}
+	}
+	return b.bytes,nil
 }
