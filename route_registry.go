70b48dadb65db07b364306278ce39b6dac7b6755
wanchao huang <hwclegend@163.com>
2022-05-27 21:09:43 +0800

    develop

--- route_registry.go
+++ route_registry.go
@@ -10,11 +10,44 @@ import (
 
 // 通过registry集中注册对象
 
-type RegistryCaller interface {
+type RegistryCaller func(c *Context, pr reflect.Value, fn reflect.Value) (interface{}, error)
+type RegistrySerialize func(ctx *Context, reply interface{}) error
+type RegistryInterface interface {
 	Caller(c *Context, fn reflect.Value) interface{}
 }
 
-type RegistrySerialize func(ctx *Context, reply interface{}) error
+type RegistryHandler struct {
+	Caller     RegistryCaller    //自定义消息调用方式
+	Serialize  RegistrySerialize //消息序列化封装
+	Middleware []MiddlewareFunc  //中间件
+}
+
+func (this *RegistryHandler) Copy(src *RegistryHandler) {
+	if src.Caller != nil {
+		this.Caller = src.Caller
+	}
+	if src.Serialize != nil {
+		this.Serialize = src.Serialize
+	}
+	if len(src.Middleware) > 0 {
+		this.Middleware = append(this.Middleware, src.Middleware...)
+	}
+}
+
+func (this *RegistryHandler) Use(src interface{}) {
+	if v, ok := src.(*RegistryHandler); ok {
+		this.Copy(v)
+	}
+	if v, ok := src.(RegistryCaller); ok {
+		this.Caller = v
+	}
+	if v, ok := src.(RegistrySerialize); ok {
+		this.Serialize = v
+	}
+	if v, ok := src.(MiddlewareFunc); ok {
+		this.Middleware = append(this.Middleware, v)
+	}
+}
 
 //NewRegistry 创建新的路由组
 // prefix路由前缀
@@ -26,18 +59,18 @@ func NewRegistry(prefix string, opts *registry.Options) *Registry {
 	if opts.Filter == nil {
 		opts.Filter = r.filter
 	}
-	r.Registry = registry.New(opts)
-	r.middleware = make(map[string][]MiddlewareFunc)
 	r.prefix = opts.Clean(prefix)
+	r.handler = make(map[string]*RegistryHandler)
+	r.Registry = registry.New(opts)
 	return r
 }
 
 type Registry struct {
 	*registry.Registry
-	prefix     string
-	Caller     func(c *Context, pr reflect.Value, fn reflect.Value) (interface{}, error) //自定义全局消息调用
-	Serialize  RegistrySerialize                                                         //消息序列化封装
-	middleware map[string][]MiddlewareFunc
+	prefix    string
+	handler   map[string]*RegistryHandler
+	Caller    RegistryCaller    //自定义全局消息调用
+	Serialize RegistrySerialize //消息序列化封装
 }
 
 func (r *Registry) filter(pr, fn reflect.Value) bool {
@@ -57,6 +90,12 @@ func (r *Registry) filter(pr, fn reflect.Value) bool {
 
 //handle cosweb入口
 func (r *Registry) handle(c *Context, next Next) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("%v", e)
+			//logger.Error("%v", err)
+		}
+	}()
 	if c.Request.URL.Path == "" || strings.Contains(c.Request.URL.Path, ".") {
 		return next()
 	}
@@ -64,28 +103,37 @@ func (r *Registry) handle(c *Context, next Next) (err error) {
 	if r.prefix != "" {
 		urlPath = strings.TrimPrefix(urlPath, r.prefix)
 	}
-
-	route, ok := r.Match(urlPath)
+	service, ok := r.Match(urlPath)
 	if !ok {
 		return next()
 	}
-
-	pr, fn, ok := route.Match(urlPath)
+	pr, fn, ok := service.Match(urlPath)
 	if !ok {
 		return next()
 	}
-	name := route.Name()
-	if err, ok = c.doMiddleware(r.middleware[name]); err != nil || !ok {
-		return
+	name := service.Name()
+	handler := r.handler[name]
+	if handler != nil && len(handler.Middleware) > 0 {
+		if err, ok = c.doMiddleware(handler.Middleware); err != nil || !ok {
+			return
+		}
 	}
 
 	var reply interface{}
-	reply, err = r.caller(c, pr, fn)
-
+	if handler != nil && handler.Caller != nil {
+		reply, err = handler.Caller(c, pr, fn)
+	} else if r.Caller != nil {
+		reply, err = r.Caller(c, pr, fn)
+	} else {
+		reply, err = r.caller(c, pr, fn)
+	}
 	if err != nil {
 		return
 	}
-	if r.Serialize != nil {
+
+	if handler != nil && handler.Serialize != nil {
+		return handler.Serialize(c, reply)
+	} else if r.Serialize != nil {
 		return r.Serialize(c, reply)
 	} else {
 		return c.JSON(reply)
@@ -93,19 +141,10 @@ func (r *Registry) handle(c *Context, next Next) (err error) {
 }
 
 func (r *Registry) caller(c *Context, pr, fn reflect.Value) (reply interface{}, err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			err = fmt.Errorf("%v", e)
-			//logger.Error("%v", err)
-		}
-	}()
-	if r.Caller != nil {
-		return r.Caller(c, pr, fn)
-	}
 	if !pr.IsValid() {
 		f, _ := fn.Interface().(func(c *Context) interface{})
 		reply = f(c)
-	} else if s, ok := pr.Interface().(RegistryCaller); ok {
+	} else if s, ok := pr.Interface().(RegistryInterface); ok {
 		reply = s.Caller(c, fn)
 	} else {
 		ret := fn.Call([]reflect.Value{pr, reflect.ValueOf(c)})
@@ -114,20 +153,22 @@ func (r *Registry) caller(c *Context, pr, fn reflect.Value) (reply interface{}, 
 	return
 }
 
-func (r *Registry) Service(name string, middleware ...MiddlewareFunc) *registry.Service {
-	route := r.Registry.Service(name)
+func (r *Registry) Service(name string, middleware ...interface{}) *registry.Service {
+	service := r.Registry.Service(name)
 	if len(middleware) > 0 {
-		s := route.Name()
-		r.middleware[s] = append(r.middleware[s], middleware...)
+		handler := &RegistryHandler{}
+		for _, m := range middleware {
+			handler.Use(m)
+		}
+		r.handler[service.Name()] = handler
 	}
-	return route
+	return service
 }
 
 //Handle 注册服务器
 func (r *Registry) Handle(s *Server, method ...string) {
-	r.Range(func(name string, _ *registry.Service) bool {
-		route := path.Join(r.prefix, name, "*")
+	for _, service := range r.Registry.Services() {
+		route := path.Join(r.prefix, service.Name(), "*")
 		s.Register(route, r.handle, method...)
-		return true
-	})
+	}
 }
